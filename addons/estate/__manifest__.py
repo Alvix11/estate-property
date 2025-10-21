8981ab2640ec34cf400eb6ac54b8c9d0ae6219b5
Alvix11 <alvix.wick@gmail.com>
2025-10-21 11:06:14 -0400

    first commit

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'author': 'Alvix.exe',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
