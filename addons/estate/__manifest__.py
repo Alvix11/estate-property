5a6a088e6e353362c5922d63c061259c39df4676
Alvix11 <alvix.wick@gmail.com>
2025-10-21 14:38:11 -0400

    chore: Update manifest and add estate_property_type view

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name': 'estate',
-    'version': '1.0',
-    'author': 'Alvix.exe',
-    'license': 'LGPL-3',
-    'depends': ['base'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_menus.xml',
+    "name": "estate",
+    "version": "1.0",
+    "author": "Alvix.exe",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_menus.xml",
     ],
-    'application': True,
-    'installable': True,
+    "application": True,
+    "installable": True,
 }
