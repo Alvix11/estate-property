4b51cf562bb6337f17f8fa9b5262e81009750c2f
Alvix11 <alvix.wick@gmail.com>
2025-10-24 12:34:01 -0400

    feat: Add constraint to prevent duplicate names in the estate_property_tag model

--- addons/estate/models/estate_property_tag.py
+++ addons/estate/models/estate_property_tag.py
@@ -4,5 +4,9 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Tag"
+    _sql_constraints = [
+        ("unique_name", "UNIQUE(name)",
+        "The name must be unique.")
+    ]
     
     name = fields.Char(required=True)
