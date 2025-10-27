e9d73324f992412b6eb8ce7c9fedcd4336610e39
Alvix11 <alvix.wick@gmail.com>
2025-10-27 11:45:35 -0400

    feat: Add _order = "name desc" to the estate_property_type model

--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type"
+    _order = "name desc"
     _sql_constraints = [
         ("unique_name", "UNIQUE(name)",
         "The name must be unique.")
