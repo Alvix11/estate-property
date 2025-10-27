34bdf1cc86875ec39729dfe7d12ac488ae32727a
Alvix11 <alvix.wick@gmail.com>
2025-10-27 11:45:19 -0400

    feat: Add _order = "name desc" to the estate_property_tag model

--- addons/estate/models/estate_property_tag.py
+++ addons/estate/models/estate_property_tag.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Tag"
+    _order = "name desc"
     _sql_constraints = [
         ("unique_name", "UNIQUE(name)",
         "The name must be unique.")
