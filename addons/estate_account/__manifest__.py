d838fa4c41d93c1c401f52578fe135a2bf716352
Alvix11 <alvix.wick@gmail.com>
2025-10-29 14:56:34 -0400

    feat: Create manifest to estate_account

---
+++ addons/estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "estate_account",
+    "version": "1.0",
+    "author": "Alvix.exe",
+    "license": "LGPL-3",
+    "depends": ["estate", "account"],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
