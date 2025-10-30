45a421c32c7a73ea59c30217e36a1e086b29fea4
Alvix11 <alvix.wick@gmail.com>
2025-10-30 12:25:37 -0400

    feat: Override action_set_sold method to create an invoice for the customer

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -1,6 +1,6 @@
 from odoo import models
 from odoo.exceptions import UserError
-
+from odoo import Command
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
@@ -8,5 +8,22 @@ class EstateProperty(models.Model):
     def action_set_sold(self):
         if self.state != "offer accepted":
             raise UserError("You cannot sell the property if you have not accepted an offer.")
+        
+        invoice = self.env["account.move"].create({
+            "partner_id": self.buyer_id.id,
+            "move_type": "out_invoice",
+            "invoice_line_ids": [
+                Command.create({
+                    "name": f"Sales commission for {self.name}",
+                    "quantity": 1,
+                    "price_unit": self.selling_price * 0.06
+                }),
+                Command.create({
+                    "name": "Administrative expenses",
+                    "quantity": 1,
+                    "price_unit": 100
+                })
+            ]
+        })
         
         return super().action_set_sold()
